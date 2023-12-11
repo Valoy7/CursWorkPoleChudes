216a160eb2378989f6d9dd78d26ae0363d781ec5
Valoy7 <Shirinskaya.2003@mail.ru>
2023-12-12 02:21:49 +0700

    Добавлена работа с БД немного

--- src/main/java/DB/Main.java
+++ src/main/java/DB/Main.java
@@ -1,9 +1,5 @@
 package DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         String quest = "проба";
---
+++ src/main/java/sample/game/AdminLogInController.java
@@ -0,0 +1,35 @@
+package sample.game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AdminLogInController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button adminLogInButton;
+
+    @FXML
+    private Button authLogInButton;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private TextField password_field;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
