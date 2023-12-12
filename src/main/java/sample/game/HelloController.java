08b01e55eea9f8b77ebb36c083edd334c2466341
Valoy7 <Shirinskaya.2003@mail.ru>
2023-12-12 17:32:38 +0700

    Добавлена вторая страничка

--- src/main/java/sample/game/HelloController.java
+++ src/main/java/sample/game/HelloController.java
@@ -1,10 +1,15 @@
 package sample.game;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
 
@@ -27,15 +32,28 @@ public class HelloController {
     private TextField password_field;
 
     @FXML
-    private Button signInButton;
+    private Button signUpButton;
 
     @FXML
     void initialize() {
+        signUpButton.setOnAction(actionEvent -> {
+            signUpButton.getScene().getWindow().hide();
+            // всё это для отображения нужного окна
+            FXMLLoader loader =  new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/game/SignUp.fxml"));
 
-        authLogInButton.setOnAction(actionEvent -> {
-            System.out.println("Вы нажали на кнопку Войти!");
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            // root параметр, который нужно подключить
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
-
     }
 
 }
---
+++ src/main/java/sample/game/SignUpController.java
@@ -0,0 +1,34 @@
+package sample.game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField login_field_register;
+
+    @FXML
+    private TextField password_field_register;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUp_ReturnLobbyButton;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
