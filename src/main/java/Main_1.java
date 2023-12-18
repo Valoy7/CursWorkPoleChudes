c493c52b9e811504cf843e6cfbc33455b11210c5
Valoy7 <Shirinskaya.2003@mail.ru>
2023-12-18 12:57:03 +0700

    улучшенный вход добавлен в экзе

--- src/main/java/Main_1.java
+++ src/main/java/Main_1.java
@@ -1,7 +1,7 @@
-//import sample.game.HelloApplication;
-//
-//public class Main_1 {
-//    public static void main(String[] args) {
-//        HelloApplication.main(args);
-//    }
-//}
+import sample.game.HelloApplication;
+
+public class Main_1 {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
